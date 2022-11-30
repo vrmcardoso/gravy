f2a4a45785ed64f550e3f864dc86da4693155d73
miguel silva <miguelvdsilva@gmail.com>
2022-11-30 10:10:59 +0000

    try to fix my version

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_160933) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_162112) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,8 +64,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_160933) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
     t.index ["category_id"], name: "index_dishes_on_category_id"
     t.index ["restaurant_id"], name: "index_dishes_on_restaurant_id"
   end
