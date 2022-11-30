d2dae23febb29e12d068f41823c2415eb489fd6f
Havard Steffensen <havardsteffensen@me.com>
2022-11-30 16:56:24 +0000

    ok

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,6 @@ class CategoriesController < ApplicationController
   def top
     @dishes = Dish.all.order("sum_points DESC")
     @restaurants = Restaurant.all
-    #@dishes_bolognese = Dish.all.where(category_id: 6).order("sum_points DESC")
   end
+
 end
