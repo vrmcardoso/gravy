a1dc037b231cc52e27a54004ea9e5a923ae29d51
vascocorreia <vascocorreia99@gmail.com>
2022-12-06 09:33:48 +0000

    seeds done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,7 +5,6 @@ class Restaurant < ApplicationRecord
 
   validates :name, presence: true
   validates :name, uniqueness: true
-  validates :description, presence: true
   validates :address, presence: true
 
 
